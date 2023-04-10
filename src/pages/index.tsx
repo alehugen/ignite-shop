fe482875ba63b28b3fcce8c4daa6c5ea8a57f615
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-10 09:35:48 -0300

    feat: create css for home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
+import camiseta1 from '@/assets/shirt_01.png'
+
 
 const Button = styled('button', {
   backgroundColor: "Azure",
@@ -6,6 +10,21 @@ const Button = styled('button', {
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={640} height={520} alt='produto'></Image>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 99.99</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} width={640} height={520} alt='produto'></Image>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$ 99.99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
