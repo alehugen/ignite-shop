aac163abf084932f64a45f59dd7fe3bdf3721ecd
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-10 08:20:14 -0300

    chore: config global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -8,10 +8,19 @@ export const {
   getCssText,
   theme,
   createTheme,
-  css } = createStitches({
-    theme: {
-      colors: {
+  css
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#FFF',
 
-      }
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
     }
-  })
+  }
+})
